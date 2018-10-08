4510947641ec3149b842fbedc2ff18f81af42267
Faris Habib <faris.habib.7@gmail.com>
2018-10-07 19:39:46 -0700

    Still figuring out the error. Rewinding time.

--- src/config/youtube.js
+++ src/config/youtube.js
@@ -1,4 +1,4 @@
 // Put your YouTube API keys here!
-const YOUTUBE_API_KEY = 'AIzaSyDhvi3wsPbqhBCoq2nrD7GKnfbwRuN5htI';
+var YOUTUBE_API_KEY = 'AIzaSyDhvi3wsPbqhBCoq2nrD7GKnfbwRuN5htI';
 
 export default YOUTUBE_API_KEY;
